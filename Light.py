d3cc999403286c032624adf357d6f9f5f33b4c40
Amanda-Clark <mandypandy22@gmail.com>
2014-11-16 15:15:11 -0800

    Added retrieving initial state and colorloop

--- Light.py
+++ Light.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request
 import requests
 import json
 
@@ -40,11 +40,12 @@ def hue():
 def sat():
     sat=request.form['sat']
     lights = request.form['light']
+
     if lights == "All":
         lights =0
     payload = {'sat': int(sat), 'transitiontime': 0}
     if lights != 0:
-        r = requests.put("http://10.1.11.222/api/amandapanda/lights/"+light+"/state/", data = json.dumps(payload))
+        r = requests.put("http://10.1.11.222/api/amandapanda/lights/"+lights+"/state/", data = json.dumps(payload))
     else:
         r = requests.put("http://10.1.11.222/api/amandapanda/groups/0/action", data = json.dumps(payload))
     content = json.loads(r.content.decode())
